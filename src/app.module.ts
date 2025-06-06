153c1b41cfa33956af37b7a55a6be5bbc6e6facd
iRogier <rhpadilla@sudamericano.edu.ec>
2025-06-06 00:01:54 -0500

    five commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { NotificationsModule } from './notifications/notifications.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { NotificationsModule } from './notifications/notifications.module';
-import { Notification } from './notifications/notification.entity';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'gjZgudwgLLZyWGCFxsgNMBbQPSiOmGOa',
-    database: 'railway',
-    entities: [Notification],
-    synchronize: true,
-  }), NotificationsModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      port: 5432,
+      username: 'postgres',
+      password: 'UncArnNeYNmUUZyTbrtimpwiqOraxTyD',
+      database: 'railway',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    NotificationsModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
