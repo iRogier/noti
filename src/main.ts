75ac06dd872d28d252bb4160c58c8526dd388e27
iRogier <rhpadilla@sudamericano.edu.ec>
2025-06-05 22:22:38 -0500

    third commit

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true }); // 👈 habilita CORS aquí
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: '*', 
+    },
+  });
+
+
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
--- src/notifications/notifications.service.ts
+++ src/notifications/notifications.service.ts
@@ -45,5 +45,5 @@ export class NotificationsService {
 
   async findAll() {
     return this.notificationRepository.find();
-  }
+    }
 }
