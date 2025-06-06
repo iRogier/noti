9ab9acb8d551f9034ea81308866d89224cc58852
iRogier <rhpadilla@sudamericano.edu.ec>
2025-06-06 00:08:39 -0500

    six commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,11 +10,7 @@ import { AppService } from './app.service';
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      url: process.env.DATABASE_URL,
-      port: 5432,
-      username: 'postgres',
-      password: 'UncArnNeYNmUUZyTbrtimpwiqOraxTyD',
-      database: 'railway',
+      url: process.env.DATABASE_URL, 
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
